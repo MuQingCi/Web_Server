4ec3181541b3ba51521af11c7c6550ce646ca0a9
Autumn_Luna <691786631@qq.com>
2026-03-11 22:39:58 +0800

    删除用于调试的LOG_INF

--- src/main.cpp
+++ src/main.cpp
@@ -16,31 +16,17 @@ int main(int argc, char* argv[])
                  config.OPT_LINGER, config.TRIGMode, config.sql_num, 
                  config.thread_num);
 
-    std::cout<<"init() run successfully!"<< std::endl;
-
     server.log_write();
-
-    std::cout<<"log_write() run successfully!"<< std::endl;
 
     server.sql_pool();
 
-    std::cout<<"sql_pool() run successfully!"<< std::endl;
-
     server.thread_pool();
-
-    std::cout<<"thread_pool() run successfully!"<< std::endl;
 
     server.trig_mode();
 
-    std::cout<<"trig_mode() run successfully!"<< std::endl;
-
     server.eventListen();
-    
-    std::cout<<"eventListen() run successfully!"<< std::endl;
 
     server.eventLoop();
-
-    std::cout<<"eventLoop() run successfully!"<< std::endl;
 
     return 0;
 }
